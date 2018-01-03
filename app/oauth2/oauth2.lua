9c5377311615b0daf3a770a5a879da4ca518a8f1
seassoon <yuezy@qq.cn>
2018-01-03 19:30:13 +0800

    add oauth2 lua file

---
+++ app/oauth2/oauth2.lua
@@ -0,0 +1,21 @@
+local _M = {}
+
+local cjson = require "cjson.safe"
+cjson.encode_empty_table_as_object(false)
+
+local mt = {
+	__index = _M
+}
+
+
+local _conf = nil
+
+local function code_url()
+	local params = ngx.encode_args({
+		client_id = _conf.client_id,
+		redirect_url = _conf.redirect_uri,
+		scope = _conf.scope
+	})
+	return _conf.code_endpoint .. "?" .. params
+end
+
