8dd2271de5462f814a84c271eb8f73a9d4b2326a
seassoon <yuezy@qq.cn>
2018-01-04 15:19:04 +0800

    雏形

--- app/conf/error_constants.lua
+++ app/conf/error_constants.lua
@@ -1,7 +1,8 @@
 return {
-    METHOD_NOT_ALLOWED = "Method Not Allowed",
+    METHOD_NOT_ALLOWED_ERROR = "Method Not Allowed",
     USERNAME_OR_PASSWORD_ERROR = "Username Or Password Error",
 
     BAD_REQUEST_ERROR = "Bad Request Error",
-    TOKEN_NOT_EXISTS_IN_HEADER = "Token Does Not Exists In Header"
+    TOKEN_NOT_EXISTS_IN_HEADER_ERROR = "Token Does Not Exists In Header",
+    UNAUTHORIZED_ERROR = "Unauthorized Error"
 }
--- app/conf/http_common.lua
+++ app/conf/http_common.lua
@@ -10,10 +10,11 @@ local _M = {}
 function _M.filter_post_method()
     if ngx.req.get_method() ~= "POST" then
         ngx.status = stats.HTTP_METHOD_NOT_ALLOWED
-        ngx.log(ngx.ERR,constants.METHOD_NOT_ALLOWED)
+        ngx.log(ngx.ERR,constants.METHOD_NOT_ALLOWED_ERROR)
         return
     end
 end
+
 
 --[[
     创建枚举的类型的方法
@@ -27,6 +28,10 @@ function _M.create_enum_table(tbl,index)
     return enum_tab
 end
 
+
+--[[
+    获取请求体
+]]
 function _M.accquire_body()
     local body = ngx.req.read_body()
     local body_raw = ngx.req.get_body_data()
@@ -41,6 +46,9 @@ function _M.accquire_body()
     end
 end
 
+--[[
+    从请求头中获取token
+]]
 function _M.get_token_form_header()
     local headers = ngx.req.get_headers()
     local token = headers['token']
@@ -50,8 +58,27 @@ function _M.get_token_form_header()
         ngx.log(ngx.ERR,constants.BAD_REQUEST_ERROR)
         return
     else
-        ngx.log(ngx.ERR,constants.TOKEN_NOT_EXISTS_IN_HEADER)
+        ngx.log(ngx.ERR,constants.TOKEN_NOT_EXISTS_IN_HEADER_ERROR)
         return
     end
 end
+
+
+--[[
+    验证token是否正确
+]]
+function _M.verify_token(token)
+    local jwt_obj = jwt:verify(vars.jwt_salt(),token)
+
+    if not jwt_obj['verified'] then
+        ngx.stats = stats.HTTP_UNAUTHORIZED_ERROR
+        ngx.log(ngx.ERR,constants.HTTP_UNAUTHORIZED_ERROR)
+        return
+    end
+    -- body
+end
+
+--[[
+    很多东西需要考虑.比如跨域问题, 静态文件的代理问题
+]]
 return _M
