60712aa8557b2bbd18f3ecb18c543104196484ee
seassoon <yuezy@qq.cn>
2018-01-06 13:31:22 +0800

    调整目录结构

--- app/plugins/basic_auth/basic_auth.lua
+++ app/plugins/basic_auth/basic_auth.lua
@@ -1,1 +1,7 @@
-local base_api = require ""
+local base_api = require "seassoon.app.plugins.basic_api"
+local common_api = require "seassoon.app.common_api"
+local api = base_api:new("basic_auth_api",2)
+
+api:merge_apis(common_api("basic_auth"))
+
+return api
